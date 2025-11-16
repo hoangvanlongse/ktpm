1953d13150a5fff576d7036d72c0bf14de93c4bd
hoangvanlongse <hoangvanlong.se@gmail.com>
2025-11-16 19:53:57 +0700

    add: get user & intructor profile by id

--- study/src/main/java/service/study/business/usecase/intructor/InstructorProfileUseCase.java
+++ study/src/main/java/service/study/business/usecase/intructor/InstructorProfileUseCase.java
@@ -4,6 +4,13 @@ import service.study.access.entity.Instructor;
 
 public interface InstructorProfileUseCase {
 	/**
+	 * Lấy thông tin người dạy theo ID
+	 * @param instructorId ID của người dạy
+	 * @return instructor
+	 */
+	Instructor getById(Long instructorId);
+
+	/**
 	 * Tạo người dạy mới
 	 * @param instructor Instructor
 	 * @return ID của người dạy
--- study/src/main/java/service/study/business/usecase/student/StudentProfileUseCase.java
+++ study/src/main/java/service/study/business/usecase/student/StudentProfileUseCase.java
@@ -3,6 +3,14 @@ package service.study.business.usecase.student;
 import service.study.access.entity.Student;
 
 public interface StudentProfileUseCase {
+
+	/**
+	 * Lấy thông tin học sinh theo ID
+	 * @param studentid ID của học sinh
+	 * @return student
+	 */
+	Student getById(Long studentId);
+
 	/**
 	 * Tạo học sinh mới
 	 * @param student Student
